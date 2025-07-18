9de99c6a360f8e3cd672096f434f59962c974a09
BrtKST <b.kostuj1011@gmail.com>
2025-07-18 11:30:13 +0200

    Update zadanie2_1.py

--- matura_korepetycje/matura2025/zadanie2_1.py
+++ matura_korepetycje/matura2025/zadanie2_1.py
@@ -1,4 +1,4 @@
-with open('C:\okienka\Python_cwiczenia\matura_korepetycje\matura2025\symbole_przyklad.txt', 'r') as file:
+with open('C:\okienka\Python_cwiczenia\matura_korepetycje\matura2025\symbole.txt', 'r') as file:
     lines = [line.strip() for line in file]
 #print(lines)
 
@@ -7,16 +7,18 @@ palindrom = [line for line in lines if line == line[::-1]]
 print(f'Odpowiedz do zadania pierwszego: {palindrom}')
 
 #zadanie 2.2
-counter = [0,0,0]
-for l in range(0,int(len(lines))-2):
-    for i in range(2,int(len(lines[l]))):
+results = []
+
+for l in range(len(lines) - 2):
+    for i in range(2, len(lines[l])):
+        # sprawdzanie czy 3 w rzedzie znaki sa takie same, potem rzedy ponizej
         if lines[l][i] == lines[l][i-1] == lines[l][i-2]:
-            if lines[l+1][i] == lines[l+1][i-1] == lines[l+1][i-2] and lines[l+2][i] == lines[l+2][i-1] == lines[l+2][i-2]:
-                counter[0] += 1
-                if counter[0] <= 1:
-                    counter[1] = (l + 1) + 1 # l+1 bo index pierwszy = 0, a liczymy rzedy od 1 oraz kolejne +1 bo szukamy srodka kwadratu
-                    counter[2] = i  
+            if (lines[l+1][i] == lines[l+1][i-1] == lines[l+1][i-2] == lines[l][i] and
+                lines[l+2][i] == lines[l+2][i-1] == lines[l+2][i-2] == lines[l][i]):
+                # środek kwadratu: rząd l+1 (jeszcze dodatkowe +1 ze względu na indeksowanie od 0), kolumna i-1 (licząc od 1)
+                results.append((l + 1 + 1, i))
 
-
-print(counter)
+print(len(results))
+for rzad, kolumna in results:
+    print(rzad, kolumna)
 
