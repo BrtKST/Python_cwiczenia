0c5815ddc50e3560b6b0ca2caa701c20a6fadfb1
BrtKST <b.kostuj1011@gmail.com>
2025-07-21 14:34:33 +0200

    Update zad3_1.py

--- matura_korepetycje/Dane-NF-2405/zad3_1.py
+++ matura_korepetycje/Dane-NF-2405/zad3_1.py
@@ -24,6 +24,7 @@ with open('C:\okienka\Python_cwiczenia\matura_korepetycje\Dane-NF-2405\skrot.txt
 print(lines)
 lines = [line.rstrip('\n') for line in lines]
 print(lines)
+file.close()
 
 #szukamy ile jest liczb dla których nie istnieje nieparzysty skrót oraz należy podać
 # największą z nich
@@ -52,3 +53,28 @@ for element in lines:
             maximum = element
 
 print(f'Największy skrót: {maximum}. W sumie w pliku wystepuje {counter} skrótów, które nie mają nieparzystego skrótu.')
+
+#zadanie 3.3.
+
+with open('C:\okienka\Python_cwiczenia\matura_korepetycje\Dane-NF-2405\skrot2.txt', 'r') as file1:
+    lines1 = file1.readlines()
+print(lines1)
+lines1 = [line.rstrip('\n') for line in lines1]
+print(lines1)
+file1.close()
+
+def wspolny_dzielnik(a,b):
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+
+new_list = list()
+for element in lines1:
+    value = skrot_ret(element)
+    if wspolny_dzielnik(int(value),int(element)) == 7:
+        new_list.append(element)
+
+print('Liczby i skróty, których NWD = 7:')
+for element in new_list:
+    print(element)
